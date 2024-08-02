e77f82910d29ba993a584d9597730da28d399f38
Daniel Dourado Mota <danieldourado2102@gmail.com>
2024-08-01 21:11:40 -0400

    adicionando views

---
+++ views.py
@@ -0,0 +1,5 @@
+def menu_principal():
+    print ('1- Criar cachorro\n2- Criar humano\n3- Listar cachorros\n4- Listar humanos\n99- Fechar programa')
+
+def menu_cao():
+    print ('1- Andar\n2- Dormir\n3- Comer\n 4-Latir\n99- Fechar Menu')
